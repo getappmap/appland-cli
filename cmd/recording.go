fb051e0a5a374a5e3a9a8f846ccd5f380f487b37
jaredKreppein <59896799+jaredKreppein@users.noreply.github.com>
2020-07-12 20:01:03 -0600

    Update recording.go

--- cmd/recording.go
+++ cmd/recording.go
@@ -16,8 +16,8 @@ func init() {
 	var (
 		recordingCmd = &cobra.Command {
 			Use:   "appmap",
-      		Short: "Manage AppMap recordings",
-      		Run: func(cmd *cobra.Command, args []string) {
+      			Short: "Manage AppMap recordings",
+      			Run: func(cmd *cobra.Command, args []string) {
 				cmd.Usage()
 			},
 		}
@@ -31,20 +31,20 @@ func init() {
 			Args:  cobra.ExactArgs(1),
 			Run: func(cmd *cobra.Command, args []string) {
 				url := args[0] + path
-                response, err := http.Post(url,"",nil)
-                if err != nil {
+                		response, err := http.Post(url,"",nil)
+                		if err != nil {
 					fail(err)
-                }
+				}
 				// best practice to defer close response.body
 				// what if no body exists?
 				defer response.Body.Close()
 
-                switch response.StatusCode {
-                case 200:
-                	fmt.Println("A new recording session has started")
-                case 409:
-                	fmt.Println("An existing recording session is already in progress")
-                default:
+                		switch response.StatusCode {
+                		case 200:
+                			fmt.Println("A new recording session has started")
+				case 409:
+					fmt.Println("An existing recording session is already in progress")
+				default:
 					fail(fmt.Errorf("An unexpected error occured, status code: %v", response.StatusCode))
 				}
 			},
@@ -60,62 +60,62 @@ func init() {
 				url := args[0] + path
 				client := &http.Client{}
 
-                request, err := http.NewRequest("DELETE", url, nil)
-                if err != nil {
+				request, err := http.NewRequest("DELETE", url, nil)
+				if err != nil {
 					fail(err)
-                }
-                response, err := client.Do(request)
-                if err != nil {
+				}
+				response, err := client.Do(request)
+				if err != nil {
 					fail(err)
-                }
-                defer response.Body.Close()
+				}
+				defer response.Body.Close()
 
-                switch response.StatusCode {
-                case 200:
+				switch response.StatusCode {
+				case 200:
 					fmt.Fprintln(os.Stderr, "Current recording session has stopped")
-                case 404:
+				case 404:
 					fmt.Fprintln(os.Stderr, "No active recording session to stop")
-                default:
+				default:
 					fail(fmt.Errorf("An unexpected error occured, Status Code: %v", response.StatusCode))
-                }
-                body, err := ioutil.ReadAll(response.Body)
-                if err != nil {
+				}
+				body, err := ioutil.ReadAll(response.Body)
+				if err != nil {
 					fail(err)
-                }
-                fmt.Println(string(body))
+				}
+				fmt.Println(string(body))
 			},
 		}
 		// Retrieves the current Appmap recording status (enabled or disabled)
 		// Returns a json body {"enabled": bool}
 		recordingCheckCmd = &cobra.Command {
 			Use:   "check [url]",
-      		Short: "Check the current AppMap recording status",
-      		Args:  cobra.ExactArgs(1),
-      		Run: func(cmd *cobra.Command, args []string) {
+			Short: "Check the current AppMap recording status",
+			Args:  cobra.ExactArgs(1),
+			Run: func(cmd *cobra.Command, args []string) {
 				url := args[0] + path
-                response, err := http.Get(url)
-                if err != nil {
+				response, err := http.Get(url)
+				if err != nil {
 					fail(err)
-                }
+				}
 				defer response.Body.Close()
 
-                if response.StatusCode != 200 {
+				if response.StatusCode != 200 {
 					fail(fmt.Errorf("An unexpected error occured, Status Code: %v", response.StatusCode))
-                }
-                body, err := ioutil.ReadAll(response.Body)
-                if err != nil {
+				}
+				body, err := ioutil.ReadAll(response.Body)
+				if err != nil {
 					fail(err)
-                }
+				}
 				var jsonBody map[string]bool
-                err = json.Unmarshal(body, &jsonBody)
-                if err != nil {
+				err = json.Unmarshal(body, &jsonBody)
+				if err != nil {
 					fail(err)
-                }
-                if jsonBody["enabled"] {
-                	fmt.Println("Appmap recording is currently enabled")
-                } else {
-                	fmt.Println("Appmap recording is currently disabled")
-                }
+				}
+				if jsonBody["enabled"] {
+					fmt.Println("Appmap recording is currently enabled")
+				} else {
+					fmt.Println("Appmap recording is currently disabled")
+				}
 			},
 		}
 	)
